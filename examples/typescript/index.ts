b81ab61d14aeff4d9b1710e9089f693ecab16086
Gakyeong Lee <gakyunggg297@gmail.com>
2022-09-19 13:44:48 +0900

    Remove unnecessary files

--- examples/typescript/index.ts
+++
@@ -1,9 +0,0 @@
-
-import opentype from '../../dist/opentype.module'
-console.log(opentype)
-// or
-import { load } from '../../dist/opentype.module'
-console.log(load)
-// or
-import * as mySpecialOpentype from '../../dist/opentype.module'
-console.log(mySpecialOpentype)
